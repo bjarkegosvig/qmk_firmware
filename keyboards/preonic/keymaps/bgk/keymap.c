9909a95197a3d104bb5cf23de67ad5a1bb799caf
Bjarke Gosvig Knudsen <bjarke.knudsen@bksv.com>
2020-10-10 15:57:13 +0200

    add danish letters

--- keyboards/preonic/keymaps/bgk/keymap.c
+++ keyboards/preonic/keymaps/bgk/keymap.c
@@ -29,9 +29,13 @@ enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
   GAME,
   LOWER,
-  RAISE
-};
+  RAISE,
+  KCOE,
+  KCAE,
+  KCAA
 
+};
+//
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -71,9 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAME] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    LOWER},
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_G},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_G},
+  {KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {KC_ESC, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -92,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11},
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL},
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TILD, KC_PIPE, KC_HOME, KC_END, _______},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TILD, KC_PIPE, KC_HOME, KC_END,  KC_LT},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
@@ -113,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HASH, KC_SLASH, KC_PGUP, KC_PGDN, _______},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HASH, KC_SLASH, KC_PGUP, KC_PGDN, KC_GT},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
@@ -134,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
   {_______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL},
   {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  GAME, _______,  _______, _______},
-  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
+  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, KCAE, KCOE,     KCAA,    _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 }
 
@@ -175,6 +179,66 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
+        case KCOE:
+          if( record->event.pressed)
+          {
+            /* SEND_STRING(SS_RALT("/O")); */
+              /* register_code(KC_RALT); */
+              /* tap_code(KC_SLSH); */
+              if(keyboard_report->mods & (MOD_BIT(KC_LSFT)))
+              {
+                  /* clear_mods(); */
+                  unregister_code(KC_LSFT);
+                  SEND_STRING(SS_RALT("/O"));
+                  register_code(KC_LSFT);
+              }
+                  /* tap_code16(LSFT(KC_O)); */
+              else
+                    SEND_STRING(SS_RALT("/o"));
+                  /* tap_code(KC_O); */
+              /* unregister_code(KC_RALT); */
+          };
+          break;
+        case KCAE:
+          if( record->event.pressed)
+          {
+            /* SEND_STRING(SS_RALT("/O")); */
+              /* register_code(KC_RALT); */
+              /* tap_code(KC_SLSH); */
+              if(keyboard_report->mods & (MOD_BIT(KC_LSFT)))
+              {
+                  /* clear_mods(); */
+                  unregister_code(KC_LSFT);
+                  SEND_STRING(SS_RALT("AE"));
+                  register_code(KC_LSFT);
+              }
+                  /* tap_code16(LSFT(KC_O)); */
+              else
+                    SEND_STRING(SS_RALT("ae"));
+                  /* tap_code(KC_O); */
+              /* unregister_code(KC_RALT); */
+          };
+          break;
+        case KCAA:
+          if( record->event.pressed)
+          {
+            /* SEND_STRING(SS_RALT("/O")); */
+              /* register_code(KC_RALT); */
+              /* tap_code(KC_SLSH); */
+              if(keyboard_report->mods & (MOD_BIT(KC_LSFT)))
+              {
+                  /* clear_mods(); */
+                  unregister_code(KC_LSFT);
+                  SEND_STRING(SS_RALT("oA"));
+                  register_code(KC_LSFT);
+              }
+                  /* tap_code16(LSFT(KC_O)); */
+              else
+                    SEND_STRING(SS_RALT("oa"));
+                  /* tap_code(KC_O); */
+              /* unregister_code(KC_RALT); */
+          };
+        break;
       }
-    return true;
+   return true;
 };
